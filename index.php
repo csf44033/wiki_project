c563270af70fda7c8f04dc2b2da4c1545a0c8ebc
Porter <36528848+csf44033@users.noreply.github.com>
2021-04-29 21:29:46 -0400

    Add Profile Template
    Add profile template and post fetching in index.php.

--- index.php
+++ index.php
@@ -1,40 +1,86 @@
+<!--if post request-->
+<?php
+    if (!empty($_POST["type"])){
+        $dbip = '127.0.0.1';
+        $dbuser = 'root';
+        $dbpass = '';
+        $dbname = 'practice';
+
+        //open database
+        $conn = new mysqli($dbip, $dbuser, $dbpass, $dbname);
+        if($conn->connect_error) {
+            die("connection failed: ".$conn->connect_error);
+        }
+
+        $sql="";
+        switch($_POST["type"]){
+            case "profile":
+                $first = $_POST["first"];
+                $last = $_POST["last"];
+
+                $code=<<<XML
+                <profile>
+                    <first>$first</first>
+                    <last>$last</last>
+                </profile>
+                XML;
+                $sql="INSERT INTO user (content) VALUES ($code);";
+        }
+        if ($conn->query($sql) === TRUE){
+            echo "New record created successfully";
+        }else{
+            echo "Error: ".$sql."<br>".$conn->error;
+        }
+        $conn->close();
+    }
+?>
 <html>
     <head>
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
+        <!--Templates-->
+        <div id="profile_form" class="form" style="display:none">
+            <form class="column" action="index.php" method="POST">
+                <img src="go_green.png" width="50">
+                <h2>Your Name</h2>
+                <input type="text" placeholder="first" name="first">
+                <input type="text" placeholder="last" name="last">
+                <input type="submit">
+                <input type="hidden" name="type" value="profile">
+            </form>
+        </div>
         <!--main container-->
         <div class="fit row">
             <!--User Profile-->
             <div id = "user-profile" class = "fit column">
-            <?php
-            $dbip = '127.0.0.1';
-            $dbuser = '';
-            $dbpass = '';
-            $dbname = 'practice';
+                <?php
+                $dbip = '127.0.0.1';
+                $dbuser = 'root';
+                $dbpass = '';
+                $dbname = 'practice';
 
-            //open database
-            $conn = new mysqli($dbip, $dbuser, $dbpass, $dbname);
-            if($conn->connect_error) {
-                die("connection failed: ".$conn->connect_error);
-            }
-
-            //fetch entire table
-            $sql = "SELECT * FROM user";
-            $result = $conn->query($sql);
-
-            if($result->num_rows > 0){
-                while($row = $result->fetch_assoc()){
-                    echo "id: ".$row["firstname"];
+                //open database
+                $conn = new mysqli($dbip, $dbuser, $dbpass, $dbname);
+                if($conn->connect_error) {
+                    die("connection failed: ".$conn->connect_error);
                 }
-            }else{
-                echo "0 results";
-            }
 
-            $conn->close();
-            ?>
+                //fetch entire table
+                $sql = "SELECT * FROM user";
+                $result = $conn->query($sql);
+
+                if($result->num_rows > 0){
+                    while($row = $result->fetch_assoc()){
+                        echo "id: ".$row["firstname"];
+                    }
+                }else{
+                    echo "0 results";
+                }
+
+                $conn->close();
+                ?>
             </div>
-
             <!--User Options-->
             <div id="user-options" class="column">
                 <!--User Templates-->
@@ -46,7 +92,7 @@
                     </div>
                     <!--templates-->
                     <div id="template-options" class="column options">
-                        <button>Profile</button>
+                        <button onclick="toggle('profile_form')">Profile</button>
                     </div>
                 </div>
                 <!--User notifications-->
